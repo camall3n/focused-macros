16a9b2274d45a3166740c78899c25f065b67b8cf
Cam Allen <csal@brown.edu>
2019-07-25 11:33:56 -0400

    Add mirroring test

--- test/test_cube.py
+++ test/test_cube.py
@@ -3,7 +3,18 @@ import cube.cube as cube
 c = cube.Cube()
 d = cube.Cube()
 
+# Test scramble and inverses
 c.scramble(30)
 assert c != d
 c.apply(cube.inverse_formula(c.formula))
 assert c == d
+
+# Test mirroring
+f = "R B' R' U' B' U F U' B U R B R' F'".split()
+g = cube.mirror_formula(f)
+assert cube.mirror_formula(g) == f
+c = cube.Cube()
+d = cube.Cube()
+c.apply(f)
+d.apply(g)
+assert d == c
