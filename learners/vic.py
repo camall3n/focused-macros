f97086a44abc04e4846f43ce93f80fc9f3648480
Cam Allen <csal@brown.edu>
2019-08-28 15:41:28 -0400

    Add DIAYN algorithm

--- learners/vic.py
+++ learners/vic.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 from learners import actorcritic as a2c
 from learners import classifier
-from learners.util import get_batch, reset_seeds
+from learners.util import get_batch
 
 class VIC:
     def __init__(self, env, ndim_s, n_actions, n_skills, discount, max_timesteps=6, n_units=32, lr=1e-3):
@@ -113,7 +113,8 @@ class VIC:
             s_f, trajectory = self.run_skill(skill)
 
             # Regress q(Ω|s0,s_f) towards Ω
-            loss = self.discriminator.train(get_batch([classifier.Sample((s_f-s0).detach(), skill)]))
+            batch = get_batch([classifier.Sample((s_f-s0).detach(), skill)])
+            loss = self.discriminator.train(batch)
             discrim_losses.append(loss.item())
 
             # Calculate intrinsic reward r_I (and update trajectory experiences)
@@ -136,75 +137,3 @@ class VIC:
             # TODO
 
         return discrim_losses, critic_losses, actor_losses
-
-def get_policy(vic, mdp, skill):
-    policy = np.empty((mdp.width+1, mdp.height+1), dtype=np.object)
-    for x in range(1,mdp.width+1):
-        for y in range(1,mdp.height+1):
-            if not env.mdp.is_wall(x,y):
-                s = torch.tensor((x,y)).float()
-                action = torch.argmax(vic.action_distr(s, torch.tensor(skill)).logits).item()
-                action_word = dict(enumerate(vic.env.action_map)).get(action, 'term')
-                policy[x,y] = action_word
-    return policy
-
-plt.figure()
-
-#%%
-reset_seeds(0)
-from simple_rl.tasks import FourRoomMDP
-from notebooks.simple_rl_env import SimpleGymEnv
-env = SimpleGymEnv(FourRoomMDP(12,12,goal_locs=[(12,12)]))
-env.render()
-s0 = torch.as_tensor(env.reset(), dtype=torch.float32)
-ndim_s = len(env.observation_space)
-n_actions = env.action_space.n
-n_skills = 40
-gamma = 0.99
-max_steps_per_skill = 10
-n_units = 32
-lr = 1e-3
-vic = VIC(env, ndim_s, n_actions, n_skills, gamma, max_steps_per_skill, n_units, lr)
-start_policies = [get_policy(vic, env.mdp, s) for s in range(n_skills)]
-#%%
-discrim_losses, critic_losses, actor_losses = vic.train(40000)
-
-#%%
-fig, ax = plt.subplots(2,2)
-ax = ax.flatten()
-ax[0].plot(discrim_losses)
-ax[1].plot(critic_losses)
-ax[2].plot(actor_losses)
-fig.tight_layout()
-fig.show()
-
-#%%
-# final_policies = [get_policy(vic, env.mdp, s) for s in range(n_skills)]
-#
-# for skill in range(n_skills):
-#     # env.render(start_policies[skill])
-#     env.render(final_policies[skill])
-#     print()
-
-#%%
-ncols = 3#int(np.floor(np.sqrt(n_skills)))
-nrows = int(np.ceil(n_skills/ncols))
-fig, ax = plt.subplots(nrows, ncols, figsize=(3*ncols,3*nrows))
-ax = ax.flatten()
-n_samples = 200
-for skill in range(n_skills):
-    final_states = np.zeros((env.mdp.width+1, env.mdp.height+1))
-    for i in range(n_samples):
-        vic.env.reset()
-        (x,y), _ = vic.run_skill(torch.tensor(skill))
-        final_states[x.int(),y.int()]+=1
-    ax[skill].imshow(final_states, cmap='hot', interpolation='nearest')
-    ax[skill].set_xlim([0.5, env.mdp.width+0.5])
-    ax[skill].set_ylim([0.5, env.mdp.height+0.5])
-    ax[skill].set_xticks(range(1,env.mdp.width+1,2))
-    ax[skill].set_yticks(range(1,env.mdp.height+1,2))
-    ax[skill].invert_yaxis()
-[a.axis('off') for a in ax[skill+1:]]
-fig.tight_layout()
-# plt.savefig('vic-40sk-40k.png')
-plt.show()
