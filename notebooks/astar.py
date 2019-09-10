495c677dfee14b4b57e221771b27f2a1690ed48d
Cam Allen <csal@brown.edu>
2019-09-10 18:50:44 -0400

    Fix A* search using primitive actions

--- notebooks/astar.py
+++ notebooks/astar.py
@@ -1,5 +1,6 @@
 import notebooks.priorityqueue as pq
 from collections import defaultdict
+from tqdm import tqdm
 
 class Node:
     def __init__(self, state, g_score, h_score, parent=None, action=None):
@@ -21,40 +22,71 @@ def reconstruct_path(node):
     actions = []
     while node.parent:
         states.insert(0, node.parent.state)
-        actions.insert(0, node.parent.action)
+        actions.insert(0, node.action)
         node = node.parent
     return states, actions
 
-def search(start, is_goal, step_cost, heuristic, get_successors):
+def search(start, is_goal, step_cost, heuristic, get_successors, max_transitions=0, debug_fn=None):
+    n_expanded = 0
+    n_transitions = 0
     open_set = pq.PriorityQueue()
     closed_set = set()
     g_score = defaultdict(lambda: float('inf'))
     g_score[start] = 0
     root = Node(state=start, g_score=0, h_score=heuristic(start), parent=None, action=None)
     open_set.push((root.f_score, root))
+    if debug_fn:
+        print('adding root to open set')
+        debug_fn(root.state)
     best = root
 
-    while open_set:
-        current = open_set.pop()
+    if max_transitions > 0:
+        progress = tqdm(total=max_transitions)
+    while open_set and n_transitions < max_transitions:
+        priority, current = open_set.pop()
+        n_expanded += 1
+        if debug_fn:
+            print('pulled node of open set')
+            debug_fn(current.state)
         if is_goal(current):
-            return reconstruct_path(node)
+            if debug_fn:
+                print('found goal. reconstructing path...')
+            return reconstruct_path(current) + (n_expanded, n_transitions)
 
         if current.state in closed_set:
+            if debug_fn:
+                print('node already in closed set')
             continue
+        if debug_fn:
+            print('adding node to closed set')
+            debug_fn(current.state)
         closed_set.add(current.state)
 
         if (current.h_score < best.h_score
             or (current.h_score == best.h_score
                 and current.g_score < best.g_score)):
+            if debug_fn:
+                print('found better node!')
+                debug_fn(current.state)
+                print('previous best was')
+                debug_fn(best.state)
             best = current
 
+        if debug_fn:
+            print('considering successors...')
         successors = get_successors(current.state)
+        n_transitions += len(successors)
+        if max_transitions > 0:
+            progress.update(len(successors))
         for state, action in successors:
             if state in closed_set:
                 continue
 
+            if debug_fn:
+                print('evaluating successor node')
+                debug_fn(state)
             # Found better path to `state`
-            g_score_via_current = g_score[current.state] + step_cost
+            g_score_via_current = g_score[current.state] + step_cost(action)
             if g_score_via_current < g_score[state]:
                 g_score[state] = g_score_via_current
                 # We'd like to remove any existing `state` Nodes from the heap,
@@ -65,5 +97,11 @@ def search(start, is_goal, step_cost, heuristic, get_successors):
                 # to `closed_set`.
                 neighbor = Node(state, g_score[state], heuristic(state), parent=current, action=action)
                 open_set.push((neighbor.f_score, neighbor))
+                if debug_fn:
+                    print('improved path to successor node; adding to open set')
+                    debug_fn(state)
 
-    return reconstruct_path(best)
+    if debug_fn:
+        print('no solution found; reconstructing path to best node...')
+        debug_fn(best.state)
+    return reconstruct_path(best) + (n_expanded, n_transitions)
--- test/test_astar.py
+++ test/test_astar.py
@@ -2,16 +2,38 @@ import copy
 from cube import cube
 from notebooks import astar
 
-start = cube.Cube()
-newcube = copy.deepcopy(start)
-newcube.transform('R')
-newcube.render()
+# Set up the scramble
+newcube = cube.Cube()
+scramble = 'R U L D F'.split()
+
+start = copy.deepcopy(newcube)
+start.apply(scramble)
 start.render()
 
-is_goal = lambda cube: cube == cube.Cube()
+#%% Run the search
+skills = [[a] for a in cube.Action.keys()]
+
+is_goal = lambda node: node.state == newcube
+step_cost = lambda skill: len(skill)
 heuristic = lambda cube: len(cube.summarize_effects())
-
+max_transitions = 3e4
+debug_fn = lambda cube: cube.render() if cube else None
 def get_successors(cube):
-    [copy.deepcopy(cube).transform(a) for a in cube.Action.keys]
+    return [(copy.deepcopy(cube).apply(s), s) for s in skills]
 
-astar.search(start, is_goal, 1, heuristic, get_successors)
+states, actions, n_expanded, n_transitions = astar.search(start, is_goal, step_cost, heuristic, get_successors, max_transitions)
+
+#%%
+for s in states:
+    s.render()
+#%%
+testcube = copy.deepcopy(newcube)
+testcube.apply(scramble)
+for a in actions:
+    testcube.apply(a)
+n_errors = len(testcube.summarize_effects())
+#%%
+actions
+n_expanded
+n_transitions
+n_errors
