9d2b07487ec0d147659048c513bac2b88fe82731
Cam Allen <csal@brown.edu>
2019-09-11 14:45:18 -0400

    Compare primitive actions vs expert options with A* search

--- test/test_astar.py
+++ test/test_astar.py
@@ -1,26 +1,30 @@
 import copy
 from cube import cube
 from notebooks import astar
+from cube.options import primitive, expert, random
+from cube import pattern
 
 # Set up the scramble
 newcube = cube.Cube()
-scramble = 'R U L D F'.split()
+scramble = pattern.scramble1
 
 start = copy.deepcopy(newcube)
 start.apply(scramble)
 start.render()
 
 #%% Run the search
-skills = [[a] for a in cube.Action.keys()]
+skills = primitive.actions
+models = primitive.models
 
 is_goal = lambda node: node.state == newcube
 step_cost = lambda skill: len(skill)
 heuristic = lambda cube: len(cube.summarize_effects())
-max_transitions = 3e4
+max_transitions = 3e5
 debug_fn = lambda cube: cube.render() if cube else None
 def get_successors(cube):
-    return [(copy.deepcopy(cube).apply(s), s) for s in skills]
+    return [(copy.deepcopy(cube).apply(swap_list=m), s) for s,m in zip(skills, models)]
 
+#%%
 states, actions, n_expanded, n_transitions = astar.search(start, is_goal, step_cost, heuristic, get_successors, max_transitions)
 
 #%%
@@ -34,6 +38,37 @@ for a in actions:
 n_errors = len(testcube.summarize_effects())
 #%%
 actions
+sum(len(a) for a in actions)
+n_expanded
+n_transitions
+n_errors
+
+#%%
+start = copy.deepcopy(newcube)
+start.apply(scramble)
+start.render()
+
+#%% Run the search
+skills = primitive.actions + expert.options
+models = primitive.models + expert.models
+step_cost = lambda skill: 1
+
+#%%
+states, actions, n_expanded, n_transitions = astar.search(start, is_goal, step_cost, heuristic, get_successors, max_transitions)
+
+#%%
+for s in states:
+    s.render()
+#%%
+testcube = copy.deepcopy(newcube)
+testcube.apply(scramble)
+for a in actions:
+    testcube.apply(a)
+n_errors = len(testcube.summarize_effects())
+#%%
+actions
+sum(len(a) for a in actions)
+len(actions)
 n_expanded
 n_transitions
 n_errors
