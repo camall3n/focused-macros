ca34c76605a65cc7460f73b84c07adcdf430a5b4
Cam Allen <csal@brown.edu>
2019-09-13 15:35:13 -0400

    Switch to 'with tqdm(...):' for tracking progress

--- notebooks/astar.py
+++ notebooks/astar.py
@@ -40,72 +40,66 @@ def search(start, is_goal, step_cost, heuristic, get_successors, max_transitions
         debug_fn(root.state)
     best = root
 
-    if max_transitions > 0:
-        progress = tqdm(total=max_transitions)
-    while open_set and n_transitions < max_transitions:
-        priority, current = open_set.pop()
-        n_expanded += 1
-        if debug_fn:
-            print('pulled node of open set')
-            debug_fn(current.state)
-        if is_goal(current):
+    with tqdm(total=max_transitions) as progress:
+        while open_set and n_transitions < max_transitions:
+            priority, current = open_set.pop()
+            n_expanded += 1
             if debug_fn:
-                print('found goal. reconstructing path...')
-            if max_transitions > 0:
-                progress.close()
-            return reconstruct_path(current) + (n_expanded, n_transitions)
-
-        if current.state in closed_set:
-            if debug_fn:
-                print('node already in closed set')
-            continue
-        if debug_fn:
-            print('adding node to closed set')
-            debug_fn(current.state)
-        closed_set.add(current.state)
-
-        if (current.h_score < best.h_score
-            or (current.h_score == best.h_score
-                and current.g_score < best.g_score)):
-            if debug_fn:
-                print('found better node!')
+                print('pulled node of open set')
                 debug_fn(current.state)
-                print('previous best was')
-                debug_fn(best.state)
-            best = current
+            if is_goal(current):
+                if debug_fn:
+                    print('found goal. reconstructing path...')
+                return reconstruct_path(current) + (n_expanded, n_transitions)
 
-        if debug_fn:
-            print('considering successors...')
-        successors = get_successors(current.state)
-        n_transitions += len(successors)
-        if max_transitions > 0:
-            progress.update(len(successors))
-        for state, action in successors:
-            if state in closed_set:
+            if current.state in closed_set:
+                if debug_fn:
+                    print('node already in closed set')
                 continue
+            if debug_fn:
+                print('adding node to closed set')
+                debug_fn(current.state)
+            closed_set.add(current.state)
+
+            if (current.h_score < best.h_score
+                or (current.h_score == best.h_score
+                    and current.g_score < best.g_score)):
+                if debug_fn:
+                    print('found better node!')
+                    debug_fn(current.state)
+                    print('previous best was')
+                    debug_fn(best.state)
+                best = current
 
             if debug_fn:
-                print('evaluating successor node')
-                debug_fn(state)
-            # Found better path to `state`
-            g_score_via_current = g_score[current.state] + step_cost(action)
-            if g_score_via_current < g_score[state]:
-                g_score[state] = g_score_via_current
-                # We'd like to remove any existing `state` Nodes from the heap,
-                # but removing from a heap is tricky. Instead we just add a new
-                # node, allowing duplicates to exist in the heap, and we wait
-                # for them to be pulled out in due time. Duplicates will be
-                # ignored anyway after the first instance of `state` is added
-                # to `closed_set`.
-                neighbor = Node(state, g_score[state], heuristic(state), parent=current, action=action)
-                open_set.push((neighbor.f_score, neighbor))
-                if debug_fn:
-                    print('improved path to successor node; adding to open set')
-                    debug_fn(state)
+                print('considering successors...')
+            successors = get_successors(current.state)
+            n_transitions += len(successors)
+            progress.update(len(successors))
+            for state, action in successors:
+                if state in closed_set:
+                    continue
 
-    if debug_fn:
-        print('no solution found; reconstructing path to best node...')
-        debug_fn(best.state)
-    if max_transitions > 0:
-        progress.close()
-    return reconstruct_path(best) + (n_expanded, n_transitions)
+                if debug_fn:
+                    print('evaluating successor node')
+                    debug_fn(state)
+                # Found better path to `state`
+                g_score_via_current = g_score[current.state] + step_cost(action)
+                if g_score_via_current < g_score[state]:
+                    g_score[state] = g_score_via_current
+                    # We'd like to remove any existing `state` Nodes from the heap,
+                    # but removing from a heap is tricky. Instead we just add a new
+                    # node, allowing duplicates to exist in the heap, and we wait
+                    # for them to be pulled out in due time. Duplicates will be
+                    # ignored anyway after the first instance of `state` is added
+                    # to `closed_set`.
+                    neighbor = Node(state, g_score[state], heuristic(state), parent=current, action=action)
+                    open_set.push((neighbor.f_score, neighbor))
+                    if debug_fn:
+                        print('improved path to successor node; adding to open set')
+                        debug_fn(state)
+
+        if debug_fn:
+            print('no solution found; reconstructing path to best node...')
+            debug_fn(best.state)
+        return reconstruct_path(best) + (n_expanded, n_transitions)
