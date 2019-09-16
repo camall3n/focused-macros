879ad19f9fadc89e857347ca86533149cc663d59
Cam Allen <csal@brown.edu>
2019-09-16 14:50:21 -0400

    Update planning_time script

--- results/planning_time.py
+++ results/planning_time.py
@@ -5,15 +5,14 @@ import os
 
 from util import rsync
 
-# results_dir = 'results/planning/'
-# os.makedirs(results_dir, exist_ok=True)
 rsync(source='brown:~/dev/skills-for-planning/results/planning',
       dest='results/')
+results_dir = 'results/planning/'
 primitive_results = glob.glob(results_dir+'*-primitive.pickle')
 expert_results = glob.glob(results_dir+'*-expert.pickle')
 
 #%%
-def generate_plot(filename, ax, color=None):
+def generate_plot(filename, ax, color=None, label=None):
     with open(filename, 'rb') as f:
         search_results = pickle.load(f)
     states, actions, n_expanded, n_transitions, candidates = search_results
@@ -32,15 +31,21 @@ def generate_plot(filename, ax, color=None):
     n_expanded
     n_transitions
     n_errors
-    ax.plot(x,y,c=color)
+    ax.plot(x,y,c=color, label=label)
 
 fig, ax = plt.subplots()
-ax.set_ylim([0,48])
+ax.set_title('Planning performance')
+ax.set_ylim([0,50])
 ax.set_xlim([0,1e5])
-for f in expert_results:
-    generate_plot(f, ax, 'C0')
-for f in expert_results:
-    generate_plot(f, ax, 'C1')
+ax.hlines(48,0,1e5,linestyles='dashed',linewidths=1)
+ax.set_ylabel('Number of errors remaining')
+ax.set_xlabel('Number of transitions considered')
+for i,f in enumerate(primitive_results):
+    label = None if i > 0 else 'actions only'
+    generate_plot(f, ax, 'C0', label=label)
+for i,f in enumerate(expert_results):
+    label = None if i > 0 else 'actions + expert skills'
+    generate_plot(f, ax, 'C1', label=label)
+ax.legend()
+# plt.savefig('results/plots/planning_time.png')
 plt.show()
-
-# states[-1].render()
