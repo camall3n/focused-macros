f2b77aec0f03e698084d759a7e76c010762e8021
Cam Allen <csal@brown.edu>
2020-02-01 20:53:55 -0500

    Clean up and rename skill_*.py -> macro_*.py in notebooks.cube

---
+++ notebooks/cube/macro_cleanup.py
@@ -0,0 +1,43 @@
+import glob
+import pickle
+import os
+
+from domains.cube import ACTIONS as primitive_actions
+from domains.cube.macros import expert
+import notebooks.picklefix  # pylint: disable=W0611
+
+
+def main():
+    """Clean up and store the learned macro-actions found with macro_search
+
+    Load the discovered macro-actions, remove any primitive actions, and save
+    the best N macros where N is equal to the number of expert macros.
+    """
+    version = '0.4'
+    # 0.1 is unused. (I can't remember what this one was, but it was worse than 0.2)
+    # 0.2 is the top 480 macros found, by h-score, within 1M simulator steps
+    # 0.3 is the top 480 macros found, by f-score, within 1M simulator steps
+    # 0.4 is the top 576 macros found, by h-score, within 1M simulator steps
+    results_dir = 'results/macros/cube/'
+    filename = glob.glob(results_dir+'v'+version+'-results.pickle')[-1]
+    with open(filename, 'rb') as file:
+        search_results = pickle.load(file)
+    best_n = search_results[-1]
+    best_n = [(score, [a[0] for a in macro]) for score, macro in best_n]
+
+    n_macros = len(expert.macros)
+
+    clean_macros = []
+    for _, macro in best_n:
+        if macro != [] and ' '.join(macro) not in primitive_actions:
+            clean_macros.append(macro)
+    clean_macros = clean_macros[-n_macros:]
+
+    #%% Save the results
+    os.makedirs('results/macros/cube', exist_ok=True)
+    with open('results/macros/cube/v{}-clean_skills.pickle'.format(version), 'wb') as file:
+        pickle.dump(clean_macros, file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ notebooks/cube/macro_search.py
@@ -0,0 +1,50 @@
+import copy
+import pickle
+import os
+
+from domains import cube
+from domains.cube import macros
+from notebooks import search
+
+
+def main():
+    """Search for disentangled macro-actions using A*"""
+    version = '0.4'
+    cost_mode = 'per-macro'
+    max_transitions = 1e6
+    save_best_n = 1200
+
+    newcube = cube.Cube()
+    start = cube.Cube()
+
+    actions = macros.primitive.actions
+    models = macros.primitive.models
+
+    is_goal = lambda node: False
+    step_cost = lambda macro: len(macro) if cost_mode == 'per-action' else 1
+
+    def heuristic(cube_):
+        effects = cube_.summarize_effects(baseline=newcube)
+        if len(effects) == 0:
+            return float('inf')
+        return len(effects)
+
+    def get_successors(cube_):
+        return [(copy.deepcopy(cube_).apply(swap_list=m), a) for a, m in zip(actions, models)]
+
+    #%% Run the search
+    search_results = search.astar(start=start,
+                                  is_goal=is_goal,
+                                  step_cost=step_cost,
+                                  heuristic=heuristic,
+                                  get_successors=get_successors,
+                                  max_transitions=max_transitions,
+                                  save_best_n=save_best_n)
+
+    #%% Save the results
+    os.makedirs('results/macros/cube', exist_ok=True)
+    with open('results/macros/cube/v{}-results.pickle'.format(version), 'wb') as file:
+        pickle.dump(search_results, file)
+
+if __name__ == '__main__':
+    main()
