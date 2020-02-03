dbbc739999846cbd350aadce9e6501da07b911b8
Cam Allen <csal@brown.edu>
2020-02-03 01:04:53 -0500

    Clean up and add docs for gridengine.cancel

--- gridengine/cancel.py
+++ gridengine/cancel.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
-#-------------------------------------------------------------------------
-# cancel
-#
-# This file simplifies the process of canceling jobs on the cluster.
-# It parses input arguments that describe the job/tasks that should be
-# canceled, and calls qdel with the appropriate arguments.
-#-------------------------------------------------------------------------
+"""
+gridengine.cancel
 
+This file simplifies the process of canceling jobs on the cluster.
+It parses input arguments that describe the job/tasks that should be
+canceled, and calls qdel with the appropriate arguments.
+"""
 
 import argparse
-import datetime
-import os
-import re
 import subprocess
 import sys
-import time
 
 def parse_args():
-    # Parse input arguments
-    #   Use --help to see a pretty description of the arguments
+    """Parse input arguments
+
+    Use --help to see a pretty description of the arguments
+    """
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-j','--jobid', help='The job ID to delete', type=str, default=None, required=True)
-    parser.add_argument('-t','--tasklist', help='Comma separated list of task IDs to submit (e.g. "18-22:1,26,29,34-49:1")', type=str, default=None)
-    parser.add_argument('-y','--dry_run', help="Don't actually submit jobs to grid engine", action='store_true')
+    parser.add_argument('-j','--jobid', type=str, default=None, required=True,
+                        help='The job ID to delete')
+    parser.add_argument('-t','--tasklist', type=str, default=None,
+                        help='Comma separated list of task IDs to submit '
+                             '(e.g. "18-22:1,26,29,34-49:1")')
+    parser.add_argument('-y','--dry_run', action='store_true',
+                        help="Don't actually submit jobs to grid engine")
     parser.set_defaults(dry_run=False)
     return parser.parse_args()
 args = parse_args()
@@ -38,8 +39,8 @@ def cancel(cmd):
     if not args.dry_run:
         try:
             subprocess.call(cmd, shell=True)
-        except (subprocess.CalledProcessError, ValueError) as e:
-            print(e)
+        except (subprocess.CalledProcessError, ValueError) as err:
+            print(err)
             sys.exit()
 
 if args.tasklist is None:
