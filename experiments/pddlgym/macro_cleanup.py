43321d694c39aa2082ba984da7e04e6ec9126483
Cam Allen <csal@brown.edu>
2020-08-21 16:43:02 -0400

    Update macro_cleanup to support PDDL 'typing', and save results

--- experiments/pddlgym/macro_cleanup.py
+++ experiments/pddlgym/macro_cleanup.py
@@ -15,29 +15,47 @@ Literal = pddlgym.structs.Literal
 LiteralConjunction = pddlgym.structs.LiteralConjunction
 Operator = pddlgym.parser.Operator
 
-def bind_literal(literal, binding):
+def bind_literal(literal, binding, use_typing=False):
     predicate = literal.predicate
-    new_vars = [TypedEntity('?'+binding[var], var.var_type) for var in literal.variables]
+    new_vars = [TypedEntity(binding[var], var.var_type) for var in literal.variables]
     return Literal(predicate, new_vars)
 
-def build_macro(*actions):
-    parameters = frozenset.union(*[a.parameters for a in actions])
-    net_precondition = set()
-    net_effect = set()
-    for action in actions:
-        for literal in action.precondition:
-            if (literal.negation) in net_effect:
-                msg = 'Precondition {i} not satisfied (action = {}). Literal {} is negated in effects: {}'
-                raise RuntimeError(msg.format(i, a, literal, effects))
-            if literal not in net_effect and literal not in net_precondition:
-                net_precondition.add(literal)
-        for literal in action.effect:
-            if (literal.negation) in net_effect:
-                net_effect.remove(literal.negation)
+def build_macro_operator(name, macro, primitive_operators, use_typing=False):
+    params = set()
+    typed_params = set()
+    net_preconds = set()
+    net_effects = set()
+    for step, action in enumerate(macro):
+        operator = primitive_operators[action.predicate.name]
+        binding = {param: '?'+var for (param, var) in zip(operator.params, action.pddl_variables())}
+        typed_binding = {param: '?'+var for (param, var) in zip(operator.params, action.pddl_variables_typed())}
+        for param in operator.params:
+            params.add(binding[param])
+            typed_params.add(typed_binding[param])
+        for literal in operator.preconds.literals:
+            bound_literal = bind_literal(literal, binding, use_typing=use_typing)
+            if bound_literal.inverted_anti in net_effects:
+                msg = ("Precondition not satisfied for action {}"
+                        " in step {} of macro:\n {}\n"
+                        "Literal {} is negated in macro effects: {}")
+                msg = msg.format(action, step, macro_id, bound_literal, net_effects)
+                raise RuntimeError(msg)
+            if bound_literal not in net_effects and bound_literal not in net_preconds:
+                net_preconds.add(bound_literal)
+        for literal in operator.effects.literals:
+            bound_literal = bind_literal(literal, binding)
+            if bound_literal.inverted_anti in net_effects:
+                net_effects.remove(bound_literal.inverted_anti)
             else:
-                net_effect.add(literal)
-    return action(parameters, net_precondition, net_effect)
+                net_effects.add(bound_literal)
 
+    if use_typing:
+        params = sorted(list(typed_params))
+    else:
+        params = sorted(list(params))
+    net_preconds = LiteralConjunction(sorted(list(net_preconds)))
+    net_effects = LiteralConjunction(sorted(list(net_effects)))
+    return Operator(name, params, net_preconds, net_effects)
 
 def main():
     """Clean up PDDLGym macros"""
@@ -52,7 +70,7 @@ def main():
     args = parser.parse_args()
 
     results_dir = 'results/macros/pddlgym/{}/problem-{:02d}/'.format(args.env_name, args.problem_index)
-    filenames = glob.glob(results_dir+'seed001-macros.pickle')
+    filenames = glob.glob(results_dir+'seed*-macros.pickle')
     assert(len(filenames)==1)
     filename = filenames[0]
     with open(filename, 'rb') as file:
@@ -68,42 +86,27 @@ def main():
 
     macros = []
     for macro_id, macro in enumerate(raw_macros[:args.n]):
-        params = set()
-        net_preconds = set()
-        net_effects = set()
-        for step, action in enumerate(macro):
-            operator = operators[action.predicate.name]
-            binding = {param: var for (param, var) in zip(operator.params, action.pddl_variables())}
-            for param in operator.params:
-                params.add(binding[param])
-            for literal in operator.preconds.literals:
-                bound_literal = bind_literal(literal, binding)
-                if bound_literal.inverted_anti in net_effects:
-                    msg = ("Precondition not satisfied for action {}"
-                           " in step {} of macro:\n {}\n"
-                           "Literal {} is negated in macro effects: {}")
-                    msg = msg.format(action, step, macro_id, bound_literal, net_effects)
-                    raise RuntimeError(msg)
-                if bound_literal not in net_effects and bound_literal not in net_preconds:
-                    net_preconds.add(bound_literal)
-            for literal in operator.effects.literals:
-                bound_literal = bind_literal(literal, binding)
-                if bound_literal.inverted_anti in net_effects:
-                    net_effects.remove(bound_literal.inverted_anti)
-                else:
-                    net_effects.add(bound_literal)
+        macro_name = 'macro{:04d}'.format(macro_id)
+        macro_operator = build_macro_operator(macro_name, macro,
+                                              primitive_operators=operators,
+                                              use_typing=env.domain.uses_typing)
+        macros.append(macro_operator)
 
-        params = sorted(list(params))
-        net_preconds = LiteralConjunction(sorted(list(net_preconds)))
-        net_effects = LiteralConjunction(sorted(list(net_effects)))
-        macro_operator = Operator('macro{:04d}'.format(macro_id), params, net_preconds, net_effects)
-
-        print(macro_operator.pddl_str())
-        print()
+    with open(env._domain_file, 'r') as file:
+        domain_pddl = file.read()
 
     #%% Save the results
-    # with open(results_dir+'clean_macros.pickle', 'wb') as file:
-    #     pickle.dump(macros, file)
+    # split domain file and insert macros just before final closing parenthetical
+    insertion_point = domain_pddl.rfind(')')
+    header, footer = domain_pddl[:insertion_point], domain_pddl[insertion_point:]
+    body = ''.join([macro.pddl_str().replace('\t', '  ') for macro in macros])+'\n'
+    macro_dir = env._domain_file.split('.')[0]+'/macros/'
+    os.makedirs(macro_dir, exist_ok=True)
+    problem_filename = env.problems[args.problem_index].problem_fname.split('/')[-1]
+    macro_file = macro_dir+problem_filename
+    with open(macro_file, 'w') as file:
+        file.write(header+body+footer)
+    print('Wrote macro-augmented domain to file: {}'.format(macro_file))
 
 if __name__ == "__main__":
     main()
