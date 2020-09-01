7f6f4b4a0b6ff14643ec8e437c14c125da54c7a9
Cam Allen <csal@brown.edu>
2020-09-01 02:48:00 -0400

    Update test_ipc_domains script

--- notebooks/test_ipc_domains.py
+++ notebooks/test_ipc_domains.py
@@ -1,21 +1,8 @@
+import argparse
+
 import gym, pddlgym
 
-def test_env(env_name='barman-sat14-strips'):
-    print('Testing {}... '.format(env_name), end='', flush=True)
-
-    try:
-        env = gym.make('PDDLEnv-IPC-{}-v0'.format(env_name.capitalize()))
-        state, _ = env.reset()
-        env.step(env.action_space.sample(state))
-        env.close()
-    except Exception as e:
-        print('Failed.')
-        print('  ', e)
-        print()
-    else:
-        print('Success!')
-
-for env_name in [
+env_names = [
     'barman-sat14-strips',
     'blocks',
     'depot',
@@ -38,5 +25,30 @@ for env_name in [
     'visitall-sat11-strips',
     'visitall-sat14-strips',
     'zenotravel',
-]:
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env_name', type=str, default=None, choices=env_names,
+                    help='Name of PDDL domain. If none supplied, loop through all IPC domains.')
+args = parser.parse_args()
+
+def test_env(env_name='barman-sat14-strips'):
+    print('Testing {}... '.format(env_name), end='', flush=True)
+
+    try:
+        env = gym.make('PDDLEnv-IPC-{}-v0'.format(env_name.capitalize()))
+        n_problems = len(env.problems)
+        state, _ = env.reset()
+        env.step(env.action_space.sample(state))
+        env.close()
+        print('Success!  Max problem ID: ', n_problems-1)
+    except Exception as e:
+        print('Failed.')
+        print('  ', e)
+        print()
+
+
+if args.env_name is not None:
+    env_names = [args.env_name]
+for env_name in env_names:
     test_env(env_name)
